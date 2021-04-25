facb0038ac0bc76c794680dfc3a48fe5f68b7480
Dmitry Ostrovsky <asper.ru@gmail.com>
2021-04-26 01:44:27 +0500

    added Process.waitall

--- indices_diagnostic.rb
+++ indices_diagnostic.rb
@@ -24,14 +24,23 @@ uri = URI ( [ elastic_url, "cat/indices?v" ].join )
 data = Net::HTTP.get(uri)
 
 puts 'Got the list! Processing further...'
+puts '############# Go processing! ##############'
 data.split("\n").grep(/ +open +[^ ]*#{ARGV[1]}/).each do |line|
   indice = line.split[2]
-  child_pid = fork do
+  #Thread.new do # uncomment if using new threads instead processes
+  child_pid = fork do # comment if using new threads instead processes
+    puts " -- Subprocess for #{indice} is started! 1..2..3.."
     puts "Trying to close indice: #{indice}..."  
     uri = URI ( [ elastic_url, indice, '/close' ].join )
     puts "HTTP POST to uri: #{uri}"
     req = Net::HTTP::Post.new(uri)
+    sleep(5) # to catch process in the list by 'ps aux|grep ruby'
     puts "Indice #{indice} is closed!"
-  end
+    puts " ---- Subprocess for #{indice} is finished! ..3..4..5!"
+  end  # comment if using new threads instead processes
   Process.waitall
+  # end.join # uncomment if using new threads instead processes
+  puts " == Processing #{indice}! 0..1..2..3..4..5.."
+  sleep(10) # to give time to subprocesses to be finished correctly
+  puts " ==== Processing #{indice}! 6..7..8..9..voila!"
 end
