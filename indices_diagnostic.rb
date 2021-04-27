c419f64199fa37ded7365f5d1490b88476d31124
Dmitry Ostrovsky <asper.ru@gmail.com>
2021-04-27 11:14:05 +0500

    fork experiments

--- indices_diagnostic.rb
+++ indices_diagnostic.rb
@@ -25,22 +25,28 @@ data = Net::HTTP.get(uri)
 
 puts 'Got the list! Processing further...'
 puts '############# Go processing! ##############'
+puts 'Process.pid, Process.ppid are:', Process.pid, Process.ppid
 data.split("\n").grep(/ +open +[^ ]*#{ARGV[1]}/).each do |line|
   indice = line.split[2]
-  #Thread.new do # uncomment if using new threads instead processes
-  child_pid = fork do # comment if using new threads instead processes
-    puts " -- Subprocess for #{indice} is started! 1..2..3.."
-    puts "Trying to close indice: #{indice}..."  
-    uri = URI ( [ elastic_url, indice, '/close' ].join )
-    puts "HTTP POST to uri: #{uri}"
-    req = Net::HTTP::Post.new(uri)
-    sleep(5) # to catch process in the list by 'ps aux|grep ruby'
-    puts "Indice #{indice} is closed!"
-    puts " ---- Subprocess for #{indice} is finished! ..3..4..5!"
-  end  # comment if using new threads instead processes
-  Process.waitall
-  # end.join # uncomment if using new threads instead processes
+  
+  child_pid = fork 
+    
+  if child_pid == 0 then next
+
+  puts " -- Subprocess for #{indice} is started! 1..2..3.."
+  puts "Trying to close indice: #{indice}..."  
+  uri = URI ( [ elastic_url, indice, '/close' ].join )
+  puts "HTTP POST to uri: #{uri}"
+  req = Net::HTTP::Post.new(uri)
+  sleep(5) # to catch process in the list by 'ps aux|grep ruby'
+  puts "Indice #{indice} is closed!"
+  puts " ---- Subprocess for #{indice} is finished! ..3..4..5!"
+
+  break
+end
+
   puts " == Processing #{indice}! 0..1..2..3..4..5.."
   sleep(10) # to give time to subprocesses to be finished correctly
   puts " ==== Processing #{indice}! 6..7..8..9..voila!"
+  Process.waitall
 end
